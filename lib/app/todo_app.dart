fd776e8fe446c4550fc19bcc81cba456a47c68a5
augusstt06 <augusstt06@gmail.com>
2025-01-29 04:14:20 +0900

    add: provider scope

--- lib/app/todo_app.dart
+++ lib/app/todo_app.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todo_flutter/config/config.dart';
-import 'package:todo_flutter/screens/screens.dart';
+import 'package:todo_flutter/config/routes/router_provider.dart';
 
-class TodoApp extends StatelessWidget {
+// 상태 구독을 위해 ConsumerWidget 사용
+class TodoApp extends ConsumerWidget {
   const TodoApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  Widget build(BuildContext context, WidgetRef ref) {
+    final routeConfig = ref.watch(routesProvider);
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       theme: AppTheme.light,
-      home: HomeScreen(),
+      routerConfig: routeConfig,
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todo_flutter/app/todo_app.dart';
 
+// riverpod 사용을 위해 ProviderScope 사용
 void main() {
-  runApp(const TodoApp());
+  runApp(const ProviderScope(child: TodoApp()));
 }
