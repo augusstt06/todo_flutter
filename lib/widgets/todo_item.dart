75da37c9e90b185ce8bed8e4112217a413e6912c
augusstt06 <augusstt06@gmail.com>
2025-01-28 23:59:29 +0900

    fix: comment

--- lib/widgets/todo_item.dart
+++ lib/widgets/todo_item.dart
@@ -5,12 +5,17 @@ import 'package:todo_flutter/utils/utils.dart';
 import 'package:todo_flutter/widgets/circle_container.dart';
 
 class TodoItem extends StatelessWidget {
+  // 클래스 필드 변수
+  // 객체가 생성될때 한번만 초기화됨. -> 변경 불가
   const TodoItem({super.key, required this.todo, this.onCompleted});
   final Todo todo;
   final Function(bool?)? onCompleted;
 
   @override
   Widget build(BuildContext context) {
+    // Widget build 메서드안에 정의.
+    // 해당 메서드 안에서만 유효. -> 메서드가 호출될때마다 초기화.
+
     final style = context.textTheme;
     final double iconOpacity = todo.isCompleted ? 0.3 : 0.5;
     final backgroundOpacity = todo.isCompleted ? 0.1 : 0.3;
